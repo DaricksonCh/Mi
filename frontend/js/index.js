70f4a3ed96820ad8c8fec1ece809170ef961d16a
DaricksonCh <darickson.stiven96@gmail.com>
2023-11-22 17:08:34 -0500

    fetching

---
+++ frontend/js/index.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch');
+
+const fetchIndexHTML = async () => {
+  const response = await fetch("https://darickosn-igs.onrender.com/frontend/index.html");
+  const html = await response.text();
+  console.log(html);
+};
+
+fetchIndexHTML();
+
+
+
